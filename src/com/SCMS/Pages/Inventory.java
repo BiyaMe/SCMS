6ff60a1af4724c32b8be5015dd8b69b095134ae7
Biyaol Mesay <101469374+BiyaMe@users.noreply.github.com>
2023-06-05 07:48:24 +0300

    Update Inventory.java

--- src/com/SCMS/Pages/Inventory.java
+++ src/com/SCMS/Pages/Inventory.java
@@ -5,56 +5,84 @@ import javax.swing.JPanel;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.*;
+import java.sql.*;
 
-public class Inventory extends JPanel{
+public class InventoryManagementPage extends JFrame {
     private JTable inventoryTable;
     private JScrollPane scrollPane;
+public InventoryManagementPage() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "1234");
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery("SELECT * from products where id == 1");
+        ResultSet result1 = statement.executeQuery("SELECT * from products where id == 2");
+        ResultSet result2 = statement.executeQuery("SELECT * from products where id == 3");
+        ResultSet result3 = statement.executeQuery("SELECT * from products where id == 4");
+        int num=result.getInt(3) * result.getInt(7);
+        int num1=result1.getInt(3) * result1.getInt(7);
+        int num2=result1.getInt(3) * result1.getInt(7);
+        int num3=result1.getInt(3) * result1.getInt(7);
+        // Set up the JFrame
+        setTitle("Inventory Management");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    public Inventory(){
-        setBackground(Color.LIGHT_GRAY);
-         String[] columnNames = {"Item", "Quantity", "Location", "Description", "Cost per item", "Inventory value"};
+        // Create the table
+        String[] columnNames = {"Product", "Quantity", "Location", "Category", "Description", "Cost per item", "Inventory value", "Edit", "Delete"};
         Object[][] data = {
-            {"Product A", 100, "Warehouse 1", "Something good", 300, 30000},
-            {"Product B", 50, "Warehouse 2", "Something better", 400, 20000},
-            {"Product C", 75, "Warehouse 1", "Something bad", 1000, 75000},
-            {"Product D", 120, "Warehouse 3", "Something worse", 500, 60000}
+        {result.getString(2), result.getString(3), result.getString(4),result.getString(5), result.getString(6), result.getString(7), num, "Edit A", "Delete A"},
+        {result1.getString(2), result1.getString(3), result1.getString(4),result1.getString(5), result1.getString(6), result1.getString(7), num1, "Edit B", "Delete B"},
+        {result2.getString(2), result2.getString(3), result2.getString(4),result2.getString(5), result2.getString(6), result2.getString(7), num2, "Edit C", "Delete C"},
+        {result3.getString(2), result3.getString(3), result3.getString(4),result3.getString(5), result3.getString(6), result3.getString(7), num3, "Edit D", "Delete D"}
         };
-        
         DefaultTableModel model = new DefaultTableModel(data, columnNames);
         inventoryTable = new JTable(model);
 
         // Add a mouse listener to the table
         inventoryTable.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                    int row = inventoryTable.getSelectedRow();
-                    int col = inventoryTable.getSelectedColumn();
+@Override
+public void mouseClicked(MouseEvent e) {
+        int row = inventoryTable.getSelectedRow();
+        int col = inventoryTable.getSelectedColumn();
+        try {
+                if (col == 7 && inventoryTable.getValueAt(row, col).equals("Edit A")) {
+                        Edit edit = new Edit(1);
+                        //Edit.setVisible(true);
+                }else if (col == 7 && inventoryTable.getValueAt(row, col).equals("Edit B")) {
+                        Edit edit = new Edit(2);
+                        //Edit.setVisible(true);
+                }else if (col == 7 && inventoryTable.getValueAt(row, col).equals("Edit C")) {
+                        Edit edit = new Edit(3);
+                        //Edit.setVisible(true);
+                }else if (col == 7 && inventoryTable.getValueAt(row, col).equals("Edit D")) {
+                        Edit edit = new Edit(4);
+                        //Edit.setVisible(true);}
+                }else if (col == 8 && inventoryTable.getValueAt(row, col).equals("Delete A")) {
+                        Delete delete = new Delete(1);
+                }else if (col == 8 && inventoryTable.getValueAt(row, col).equals("Delete B")) {
+                        Delete delete = new Delete(2);
+                }else if (col == 8 && inventoryTable.getValueAt(row, col).equals("Delete C")) {
+                        Delete delete = new Delete(3);
+                }else if (col == 8 && inventoryTable.getValueAt(row, col).equals("Delete D")) {
+                        Delete delete = new Delete(4);
+                }
 
-                    if (col == 0 && inventoryTable.getValueAt(row, col).equals("Product A")) {
-                    ProductAPage productAPage = new ProductAPage();
-                    productAPage.setVisible(true);
-                    }else if (col == 0 && inventoryTable.getValueAt(row, col).equals("Product B")) {
-                    ProductBPage productBPage = new ProductBPage();
-                    productBPage.setVisible(true);
-                    }else if (col == 0 && inventoryTable.getValueAt(row, col).equals("Product C")) {
-                    ProductCPage productCPage = new ProductCPage();
-                    productCPage.setVisible(true);
-                    }else if (col == 0 && inventoryTable.getValueAt(row, col).equals("Product D")) {
-                    ProductDPage productDPage = new ProductDPage();
-                    productDPage.setVisible(true);
-                    }
-            }
-        });
 
-                    // Set up the scroll pane
-                    scrollPane = new JScrollPane(inventoryTable);
-                    add(scrollPane);
 
-                    // Display the JFrame
+        } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+        }
+}});
+
+        // Set up the scroll pane
+        scrollPane = new JScrollPane(inventoryTable);
+        add(scrollPane);
+
+        // Display the JFrame
         setVisible(true);
-    }
+        }
 
-public static void main(String[] args) {
+public static void main(String[] args) throws SQLException {
         InventoryManagementPage inventoryPage = new InventoryManagementPage();
         }
 }
